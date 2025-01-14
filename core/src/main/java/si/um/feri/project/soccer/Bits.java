3dc11a0110655093dba3439bf0d0db3c4451e277
david <david.lipavec02@gmail.com>
2025-01-14 11:12:23 +0100

    SCRUM-208 Added jump particle effect

--- core/src/main/java/si/um/feri/project/soccer/Bits.java
+++ core/src/main/java/si/um/feri/project/soccer/Bits.java
@@ -5,6 +5,7 @@ public final  class Bits {
     public final static short GOALSENSOR_BIT = 1;
     public final static short PLAYER_BIT = 8;
     public final static short POWERUP_BIT = 32;
+    public final static short LEG_BIT = 64;
 
 
     private Bits(){}
